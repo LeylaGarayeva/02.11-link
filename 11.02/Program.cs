6aa5c8cb85a8d441dcb43d9f52140404731cf050
Leyla Garayeva <leylagarayeva1@gmail.com>
2018-02-11 19:24:15 +0400

    first commit

---
+++ 11.02/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> students = new List<Person>();
+            students.Add(new Person("Ismayil", "Ismayilov", 29));
+            students.Add(new Person("Eli", "Allahverdiyev", 27));
+            students.Add(new Person("Yaqub", "Allahverdiyev", 27));
+            students.Add(new Person("Nicat", "Hemidov", 20));
+
+            //List<Person> wanteds = new List<Person>();
+            Person wanted = null;
+            wanted = students.FirstOrDefault(f => f.age == 21);
+            IEnumerable<Person> want = students.Where(f => f.age == 27);
+        }
+    }
+
+    class Person {
+        public string name { get; set; }
+        public string lastname { get; set; }
+        public int age { get; set; }
+        public Person(string n,string l,int a)
+        {
+
+
+            name = n;
+            lastname = l;
+            age = a;
+        }
+    }
+
+}
